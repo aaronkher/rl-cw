f3de43a52da17956f50598168ae76e84ec57decc
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2023-12-28 00:48:15 +0100

    replace epsilon decay code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         gamma=0.99,
         epsilon_start=0.9,
         epsilon_min=0.05,
-        epsilon_decay=0.02,
+        epsilon_decay=1000,
         C=1,
         buffer_batch_size=128,
     )
