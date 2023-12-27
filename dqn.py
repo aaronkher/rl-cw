1230197c457f593ddcc522c572182d723a62599f
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2023-12-27 19:33:35 +0100

    Switch to CartPole-v1 Use tau method of updating target network Adjust hyperparameters Switch to L1 (huber) loss Run black formatter

--- dqn.py
+++ dqn.py
@@ -47,10 +47,8 @@ class ExperienceBatch:
         self.terminal = NeuralNetwork.tensorify([exp.terminal for exp in experiences])
 
 
-
-
 class ReplayBuffer:
-    def __init__(self, replay_buffer_size: int,  omega: float = 0.5):
+    def __init__(self, replay_buffer_size: int, omega: float = 0.5):
         self.buffer = collections.deque(maxlen=replay_buffer_size)
         self.omega = omega
 
@@ -79,11 +77,11 @@ class ReplayBuffer:
         indicies = np.random.choice(len(self.buffer), size=batch_size, p=probabilities)
         experiences = [self.buffer[idx] for idx in indicies]
 
-        return ExperienceBatch(experiences)    
-    
+        return ExperienceBatch(experiences)
+
     def get_buffer(self):
         return ExperienceBatch(list(self.buffer))
-    
+
     def size(self) -> int:
         return len(self.buffer)
 
@@ -109,7 +107,6 @@ class DQN:
         self.decay_rate = epsilon_decay
         self.epsilon = epsilon_start
 
-
         self.gamma = gamma
         self.C = C
         self.buffer_batch_size = buffer_batch_size
@@ -120,7 +117,7 @@ class DQN:
         self.policy_network = NeuralNetwork(self.environment) # q1
         self.target_network = NeuralNetwork(self.environment) # q2
         # copy q2 to q1
-        self.policy_network.load_state_dict(self.target_network.state_dict())
+        self.target_network.load_state_dict(self.policy_network.state_dict())
 
     def get_best_action(self, state: State) -> Action:
         return self.policy_network.get_best_action(state)
@@ -181,16 +178,26 @@ class DQN:
     θ−t = target network (in our case)
     """
     def compute_td_targets_batch(self, experiences: ExperienceBatch) -> TdTargetBatch:
-        rewards = experiences.rewards.unsqueeze(1)
-            
-        # using policy network for action selection (θt)
-        max_actions = self.policy_network.get_q_values_batch(experiences.new_states).batch_output.argmax(dim=1).unsqueeze(1)
-        
-        # using target network for q value calculation (θ−t)
-        q_values = self.target_network.get_q_values_batch(experiences.new_states).batch_output
-        
-        max_q_values = q_values.gather(1, max_actions)
-        max_q_values[experiences.terminal] = 0.0  
+        # Tensor[[Reward], [Reward], ...]
+        rewards = experiences.rewards.unsqueeze(1)  # R_t+1
+
+        # get best actions according to policy network
+        # Tensor[[QValue * 3], [QValue * 3], ...]
+        policy_network_q_values = self.policy_network.get_q_values_batch(
+            experiences.new_states
+        )
+        # Tensor[Action, Action, ...]
+        best_actions = policy_network_q_values.batch_output.argmax(dim=1)
+        # Tensor[[Action], [Action], ...]
+        best_actions = best_actions.unsqueeze(1)
+
+        # calculate q values using target network
+        q_values = self.target_network.get_q_values_batch(experiences.new_states)
+
+        # get the target network's q value of the policy network's chosen action
+        max_q_values = q_values.batch_output.gather(1, best_actions)
+        # discounted reward is 0 for terminal states
+        max_q_values[experiences.terminal] = 0.0
 
         td_targets = rewards + (self.gamma * max_q_values)
 
@@ -204,13 +211,23 @@ class DQN:
         print(f"Epsilon decayed to {self.epsilon}")
 
     def update_target_network(self):
-        policy_network_weights = self.policy_network.state_dict()
-        self.target_network.load_state_dict(policy_network_weights)
+        # policy_network_weights = self.policy_network.state_dict()
+        # self.target_network.load_state_dict(policy_network_weights)
+
+        # tmp: update network using weighted average of both networks
+        target_net_state = self.target_network.state_dict()
+        policy_net_state = self.policy_network.state_dict()
+        tau = 0.05
+
+        for key in target_net_state:
+            target_net_state[key] = (
+                tau * policy_net_state[key] + (1 - tau) * target_net_state[key]
+            )
+
+        self.target_network.load_state_dict(target_net_state)
 
     def backprop(self, experiences: ExperienceBatch, td_targets: TdTargetBatch):
         self.policy_network.backprop(experiences, td_targets)
-    
-
 
     def train(self):
         episodes = []
@@ -236,8 +253,17 @@ class DQN:
                     # print(
                     #     f"Episode {episode} Timestep {timestep} | Action {action}, Reward {action_result.reward:.0f}, Total Reward {reward_sum:.0f}"
                     # )
- 
-                    td_target = self.compute_td_target(action_result.reward, action_result.new_state)
+
+                    experience_temp = Experience(
+                        action_result.old_state,
+                        action_result.new_state,
+                        action,
+                        action_result.reward,
+                        action_result.terminal and action_result.won,
+                        0.0,
+                    )
+
+                    td_target = self.compute_td_target(experience_temp)
 
                     experience = Experience(
                         action_result.old_state,
@@ -245,19 +271,18 @@ class DQN:
                         action,
                         action_result.reward,
                         action_result.terminal and action_result.won,
-                        td_target
+                        td_target,
                     )
-
 
                     self.replay_buffer.add_experience(experience)
 
-        
                     if self.replay_buffer.size() > self.buffer_batch_size:
-                        replay_batch = self.replay_buffer.prioritized_replay_sampling(self.buffer_batch_size)
+                        replay_batch = self.replay_buffer.prioritized_replay_sampling(
+                            self.buffer_batch_size
+                        )
 
                         # compute td targets for the whole of the replay batch
                         td_targets = self.compute_td_targets_batch(replay_batch)
-
 
                         self.backprop(replay_batch, td_targets)
 
