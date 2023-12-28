36fe876cf5f23f1e23cfc4c0d8de57974ef64997
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2023-12-28 19:14:39 +0100

    make our code deterministic

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import os
+import random
+
+import numpy as np
+import torch
+
 from dqn import DQN
+
+RANDOM_SEED = 3
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
+torch.use_deterministic_algorithms(True)
+# https://docs.nvidia.com/cuda/cublas/index.html#results-reproducibility
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+os.environ["PYTHONHASHSEED"] = str(RANDOM_SEED)
 
 if __name__ == "__main__":
     dqn = DQN(
