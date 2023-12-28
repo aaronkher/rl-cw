256c04e29459e2515e1318bcf580cc2e3ec572a0
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2023-12-28 18:59:29 +0100

    Make PyTorch DQN tutorial deterministic

--- references/pytorch-dqn-tutorial.py
+++ references/pytorch-dqn-tutorial.py
@@ -61,6 +61,7 @@ We'll also use the following from PyTorch:
 """
 
 import gymnasium as gym
+import os
 import math
 import random
 import matplotlib
@@ -73,10 +74,22 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+import numpy as np
+
+RANDOM_SEED = 3
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
+torch.use_deterministic_algorithms(True)
+# https://docs.nvidia.com/cuda/cublas/index.html#results-reproducibility
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+os.environ["PYTHONHASHSEED"] = str(RANDOM_SEED)
+
 env = gym.make("CartPole-v1")
+env.action_space.seed(RANDOM_SEED)
 
 # set up matplotlib
-is_ipython = 'inline' in matplotlib.get_backend()
+is_ipython = "inline" in matplotlib.get_backend()
 if is_ipython:
     from IPython import display
 
@@ -107,12 +120,10 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #    method for selecting a random batch of transitions for training.
 #
 
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
+Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
 
 
 class ReplayMemory(object):
-
     def __init__(self, capacity):
         self.memory = deque([], maxlen=capacity)
 
@@ -143,10 +154,10 @@ class ReplayMemory(object):
 # :math:`R_{t_0} = \sum_{t=t_0}^{\infty} \gamma^{t - t_0} r_t`, where
 # :math:`R_{t_0}` is also known as the *return*. The discount,
 # :math:`\gamma`, should be a constant between :math:`0` and :math:`1`
-# that ensures the sum converges. A lower :math:`\gamma` makes 
-# rewards from the uncertain far future less important for our agent 
-# than the ones in the near future that it can be fairly confident 
-# about. It also encourages agents to collect reward closer in time 
+# that ensures the sum converges. A lower :math:`\gamma` makes
+# rewards from the uncertain far future less important for our agent
+# than the ones in the near future that it can be fairly confident
+# about. It also encourages agents to collect reward closer in time
 # than equivalent rewards that are temporally far away in the future.
 #
 # The main idea behind Q-learning is that if we had a function
@@ -202,8 +213,8 @@ class ReplayMemory(object):
 # taking each action given the current input.
 #
 
-class DQN(nn.Module):
 
+class DQN(nn.Module):
     def __init__(self, n_observations, n_actions):
         super(DQN, self).__init__()
         self.layer1 = nn.Linear(n_observations, 128)
@@ -258,7 +269,7 @@ LR = 1e-4
 # Get number of actions from gym action space
 n_actions = env.action_space.n
 # Get the number of state observations
-state, info = env.reset()
+state, info = env.reset(seed=RANDOM_SEED)
 n_observations = len(state)
 
 policy_net = DQN(n_observations, n_actions).to(device)
@@ -275,8 +286,9 @@ steps_done = 0
 def select_action(state):
     global steps_done
     sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-        math.exp(-1. * steps_done / EPS_DECAY)
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(
+        -1.0 * steps_done / EPS_DECAY
+    )
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
@@ -285,7 +297,9 @@ def select_action(state):
             # found, so we pick action with the larger expected reward.
             return policy_net(state).max(1).indices.view(1, 1)
     else:
-        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+        return torch.tensor(
+            [[env.action_space.sample()]], device=device, dtype=torch.long
+        )
 
 
 episode_durations = []
@@ -295,12 +309,12 @@ def plot_durations(show_result=False):
     plt.figure(1)
     durations_t = torch.tensor(episode_durations, dtype=torch.float)
     if show_result:
-        plt.title('Result')
+        plt.title("Result")
     else:
         plt.clf()
-        plt.title('Training...')
-    plt.xlabel('Episode')
-    plt.ylabel('Duration')
+        plt.title("Training...")
+    plt.xlabel("Episode")
+    plt.ylabel("Duration")
     plt.plot(durations_t.numpy())
     # Take 100 episode averages and plot them too
     if len(durations_t) >= 100:
@@ -329,10 +343,11 @@ def plot_durations(show_result=False):
 # :math:`V(s_{t+1}) = \max_a Q(s_{t+1}, a)`, and combines them into our
 # loss. By definition we set :math:`V(s) = 0` if :math:`s` is a terminal
 # state. We also use a target network to compute :math:`V(s_{t+1})` for
-# added stability. The target network is updated at every step with a 
-# `soft update <https://arxiv.org/pdf/1509.02971.pdf>`__ controlled by 
+# added stability. The target network is updated at every step with a
+# `soft update <https://arxiv.org/pdf/1509.02971.pdf>`__ controlled by
 # the hyperparameter ``TAU``, which was previously defined.
 #
+
 
 def optimize_model():
     if len(memory) < BATCH_SIZE:
@@ -345,10 +360,12 @@ def optimize_model():
 
     # Compute a mask of non-final states and concatenate the batch elements
     # (a final state would've been the one after which simulation ended)
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                          batch.next_state)), device=device, dtype=torch.bool)
-    non_final_next_states = torch.cat([s for s in batch.next_state
-                                                if s is not None])
+    non_final_mask = torch.tensor(
+        tuple(map(lambda s: s is not None, batch.next_state)),
+        device=device,
+        dtype=torch.bool,
+    )
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
     state_batch = torch.cat(batch.state)
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
@@ -365,7 +382,9 @@ def optimize_model():
     # state value or 0 in case the state was final.
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
     with torch.no_grad():
-        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
+        next_state_values[non_final_mask] = (
+            target_net(non_final_next_states).max(1).values
+        )
     # Compute the expected Q values
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
@@ -389,10 +408,10 @@ def optimize_model():
 # 1), and optimize our model once. When the episode ends (our model
 # fails), we restart the loop.
 #
-# Below, `num_episodes` is set to 600 if a GPU is available, otherwise 50 
-# episodes are scheduled so training does not take too long. However, 50 
+# Below, `num_episodes` is set to 600 if a GPU is available, otherwise 50
+# episodes are scheduled so training does not take too long. However, 50
 # episodes is insufficient for to observe good performance on CartPole.
-# You should see the model constantly achieve 500 steps within 600 training 
+# You should see the model constantly achieve 500 steps within 600 training
 # episodes. Training RL agents can be a noisy process, so restarting training
 # can produce better results if convergence is not observed.
 #
@@ -415,7 +434,9 @@ for i_episode in range(num_episodes):
         if terminated:
             next_state = None
         else:
-            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+            next_state = torch.tensor(
+                observation, dtype=torch.float32, device=device
+            ).unsqueeze(0)
 
         # Store the transition in memory
         memory.push(state, action, next_state, reward)
@@ -431,15 +452,18 @@ for i_episode in range(num_episodes):
         target_net_state_dict = target_net.state_dict()
         policy_net_state_dict = policy_net.state_dict()
         for key in policy_net_state_dict:
-            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+            target_net_state_dict[key] = policy_net_state_dict[
+                key
+            ] * TAU + target_net_state_dict[key] * (1 - TAU)
         target_net.load_state_dict(target_net_state_dict)
 
         if done:
             episode_durations.append(t + 1)
+            print(f"Episode {i_episode} finished after {t + 1} timesteps.")
             plot_durations()
             break
 
-print('Complete')
+print("Complete")
 plot_durations(show_result=True)
 plt.ioff()
 plt.show()
